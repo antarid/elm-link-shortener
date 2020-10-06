294dc0146c1478fc9555636b484a7649e9e5a758
antarid <tarand007@gmail.com>
2020-10-06 10:12:43 +0300

    Updates

---
+++ precache-manifest.8d7ff5d76af658c454a97f77df1083bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/elm-link-shortener/favicon.ico"
+  },
+  {
+    "revision": "82bc1008f3fe16e7778421acc2150627",
+    "url": "/elm-link-shortener/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/elm-link-shortener/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/elm-link-shortener/manifest.json"
+  },
+  {
+    "revision": "bee4274279fe36c27750",
+    "url": "/elm-link-shortener/static/css/main.7eeb4bbb.chunk.css"
+  },
+  {
+    "revision": "bee4274279fe36c27750",
+    "url": "/elm-link-shortener/static/js/main.bee42742.chunk.js"
+  },
+  {
+    "revision": "1e8c51c8331d716169d0",
+    "url": "/elm-link-shortener/static/js/runtime~main.1e8c51c8.js"
+  },
+  {
+    "revision": "bb36eb2f1e66572755c6",
+    "url": "/elm-link-shortener/static/js/vendors~main.bb36eb2f.chunk.js"
+  }
+]);
